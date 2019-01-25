8c3080a6c897dbca5c3f974e15a0488d45a5e060
Josh Cohen <jacohen10@gmail.com>
2019-01-25 15:27:33 -0500

    share channel info

--- bot/bot.rb
+++ bot/bot.rb
@@ -2,4 +2,9 @@ class Bot < SlackRubyBot::Bot
   command 'say' do |client, data, match|
     client.say(channel: data.channel, text: match['expression'])
   end
+
+
+  command 'share' do |client, data, match|
+    client.say(channel: data.channel, text: client.channels_info(channel: data.channel))
+  end
 end

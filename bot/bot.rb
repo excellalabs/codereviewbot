8acf03a5cb2f5a370149208b7bdf0ea899fa0343
Josh Cohen <jacohen10@gmail.com>
2019-01-25 17:37:28 -0500

    prevent enr-cr from selecting yourself

--- bot/bot.rb
+++ bot/bot.rb
@@ -7,12 +7,17 @@ class Bot < SlackRubyBot::Bot
     slack = Slack::Web::Client.new
     members = slack.channels_info(channel: data.channel).to_hash["channel"]["members"]
     puts members
-    members.each do |user|
+    members.each do |member|
       client.say(channel: data.channel, text: member)
     end
   end
 
   command 'info' do |client, data, match|
+    puts data.to_hash
+    client.say(channel: data.channel, text: data.to_hash)
+  end
+
+  command 'user-info' do |client, data, match|
     puts data.to_hash
     client.say(channel: data.channel, text: data.to_hash)
   end
@@ -30,6 +35,8 @@ class Bot < SlackRubyBot::Bot
   operator 'enr-cr' do |client, data, match|
     slack = Slack::Web::Client.new
     user = User.order("updated_at ASC").first
+    user = User.order("updated_at ASC").second if user.name == data["user"]
+
     client.say(channel: data.channel, text: "<@#{user.name}>")
     user.touch
   end

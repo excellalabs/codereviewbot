b47c9444552b79eb179410e862c94a84f3f19bf6
Josh Cohen <jacohen10@gmail.com>
2019-01-25 16:17:43 -0500

    enr-cr and enr-cr-reset

--- bot/bot.rb
+++ bot/bot.rb
@@ -3,9 +3,24 @@ class Bot < SlackRubyBot::Bot
     client.say(channel: data.channel, text: match['expression'])
   end
 
-
   command 'share' do |client, data, match|
     slack = Slack::Web::Client.new
     client.say(channel: data.channel, text: slack.channels_info(channel: data.channel))
   end
+
+  operator 'enr-cr' do |client, data, match|
+    slack = Slack::Web::Client.new
+    user = User.order("updated_at ASC").first
+    client.say(channel: data.channel, text: user)
+    user.touch
+  end
+
+  operator 'enr-cr-reset' do |client, data, match|
+    slack = Slack::Web::Client.new
+    User.destroy_all
+    slack.channels_info(channel: data.channel).members.each do |user|
+      User.create!(name: user)
+    end
+  end
+
 end

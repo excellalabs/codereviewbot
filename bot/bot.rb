23af87fc707ad52ce50e137a1208d13b2af58cec
Josh Cohen <jacohen10@gmail.com>
2019-01-25 20:31:07 -0500

    reorder code

--- bot/bot.rb
+++ bot/bot.rb
@@ -20,21 +20,11 @@ class Bot < SlackRubyBot::Bot
   operator 'code-review-reset' do |client, data, match|
     slack = Slack::Web::Client.new
     old_users = User.where(active: true, channel: data.channel)
-    puts 'old_users: '
-    puts old_users
     old_users.destroy_all
     members = slack.channels_info(channel: data.channel).to_hash["channel"]["members"]
-    puts 'members: '
-    puts members
     members_to_exclude = User.where(active: false).pluck(:name)
-    puts 'members_to_exclude: '
-    puts members_to_exclude
-
     members.reject! { |id| members_to_exclude.include?(id) }
-    puts 'members: '
-    puts members
-    puts 'channel: '
-    puts data.channel
+
     members.each do |user|
       User.create!(name: user, channel: data.channel)
     end
@@ -49,6 +39,11 @@ class Bot < SlackRubyBot::Bot
     user.touch
   end
 
+  operator 'cr-excluded-list' do |client, data, match|
+    excluded_users = User.where(active: false, channel: data.channel).pluck(:name)
+    client.say(channel: data.channel, text: excluded_users)
+  end
+
   operator 'cr-exclude' do |client, data, match|
     excluded_members = match['expression'].split(",")
     excluded_members.each do |user|
@@ -58,10 +53,6 @@ class Bot < SlackRubyBot::Bot
     client.say(channel: data.channel, text: "#{excluded_members.count} user(s) excluded")
   end
 
-  operator 'cr-excluded-list' do |client, data, match|
-    excluded_users = User.where(active: false, channel: data.channel).pluck(:name) || 'Currently Empty'
-    client.say(channel: data.channel, text: excluded_users)
-  end
 
   operator 'andon' do |client, data, match|
     client.say(channel: data.channel, text: "<!here> CODE RED! Stop what you're doing. Find out what you can do to help.")

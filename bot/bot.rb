8c97cb082b14d1c8adbe62ba13f9935acac3a1ed
Josh Cohen <jacohen10@gmail.com>
2019-01-25 19:43:42 -0500

    added channel and active columns. also started exclude functionality

--- bot/bot.rb
+++ bot/bot.rb
@@ -3,7 +3,7 @@ class Bot < SlackRubyBot::Bot
     client.say(channel: data.channel, text: match['expression'])
   end
 
-  command 'shareMembers' do |client, data, match|
+  command 'channel-members' do |client, data, match|
     slack = Slack::Web::Client.new
     members = slack.channels_info(channel: data.channel).to_hash["channel"]["members"]
     puts members
@@ -17,24 +17,33 @@ class Bot < SlackRubyBot::Bot
     client.say(channel: data.channel, text: data.to_hash)
   end
 
-  operator 'enr-cr-reset' do |client, data, match|
+  operator 'code-review-reset' do |client, data, match|
     slack = Slack::Web::Client.new
-    User.destroy_all
+    User.where(active: true, channel: data.channel).destroy_all
     members = slack.channels_info(channel: data.channel).to_hash["channel"]["members"]
-    members_to_exclude = ENV['EXCLUDED_MEMBERS'].split(",")
-    members.reject! { |id| members_to_exclude .include?(id) }
+    members_to_exclude =  User.where(active: false).pluck(:name)
+    members.reject! { |id| members_to_exclude.include?(id) }
     members.each do |user|
-      User.create!(name: user)
+      User.create!(name: user, channel: data.channel)
     end
   end
 
-  operator 'enr-cr' do |client, data, match|
-    slack = Slack::Web::Client.new
-    user = User.order("updated_at ASC").first
-    user = User.order("updated_at ASC").second if user.name == data["user"]
+  operator 'code-review' do |client, data, match|
+    user = User.where(channel: data.channel).order("updated_at ASC").first
+    user = User.where(channel: data.channel).order("updated_at ASC").second if user.name == data["user"]
 
     client.say(channel: data.channel, text: "<@#{user.name}>")
     user.touch
+  end
+
+  operator 'cr-exclude' do |client, data, match|
+    client.say(channel: data.channel, text: match['expression'])
+  end
+
+  operator 'cr-excluded-list' do |client, data, match|
+    excluded_users = User.where(active: false, channel: data.channel) || 'Currently Empty'
+    client.say(channel: data.channel, text: match['expression'])
+    client.say(channel: data.channel, text: excluded_users)
   end
 
   operator 'andon' do |client, data, match|
